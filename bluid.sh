438875bc7122171b00570d1d0cd3d737ad051e52
Daniel17-sg <l231080108@iztapalpa.tecnm.mx>
2026-05-07 23:21:51 -0600

    Add create_admin.py and update bluid.sh for auto superuser creation on deploy

--- bluid.sh
+++ bluid.sh
@@ -2,6 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-cd vinateria_3_hermanos
 python manage.py collectstatic --noinput
-# Si tienes migraciones y DB en Render: python manage.py migrate
+python manage.py migrate
+python create_admin.py
---
+++ create_admin.py
@@ -0,0 +1,18 @@
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vinateria_3_hermanos.settings')
+django.setup()
+
+User = get_user_model()
+# Datos para tu superusuario
+username = 'admin'
+email = 'admin@example.com'
+password = '123'  # Pon una contraseña real aquí
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Superusuario '{username}' creado exitosamente.")
+else:
+    print(f"El superusuario '{username}' ya existe.")
